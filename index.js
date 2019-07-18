99ed5689de2e63801e3a1d4ceb18d98c1ef77b5a
George Chkhikvadze <gio2396ch@gmail.com>
2019-07-18 05:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string){
   string === string.toLowerCase()
   return "I can't hear you!"
 }
-//function sayHiToGrandma(string){
-  //string === string.toUpperCase()
-  //return "YES INDEED!"
-//}
+function sayHiToGrandma(string){
+  string === string.toUpperCase()
+  return "YES INDEED!"
+}
